c3244c6f270b842ca7be9a92e69b63bba7d21c24
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-03-28 16:50:09 +0530

    init

---
+++ PJX/lambda.js
@@ -0,0 +1,20 @@
+let AWS = require('aws-sdk');
+let request = require('request');
+exports.handler = function (event, context, callback) {
+	let proxyResponse = {
+		statusCode: 200,
+		headers: {
+			"x-custom-header": "my custom header value"
+		},
+		body: ''
+	};
+	var options = {
+		url: event.queryStringParameters.url,
+		headers: event.headers
+	};
+	request(event.queryStringParameters.url, function (error, response, body) {
+		proxyResponse.statusCode = response.statusCode;
+		response.body = body;
+		callback(null, response);
+	});
+}
